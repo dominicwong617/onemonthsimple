d01884676e47a685acc97a4218c64d1d6a1f6c79
Dominic Wong <dominic.wong.617@gmail.com>
2015-06-29 22:42:43 +0800

    restrict what files a user can download

--- app/controllers/benefit_forms_controller.rb
+++ app/controllers/benefit_forms_controller.rb
@@ -1,16 +1,25 @@
 class BenefitFormsController < ApplicationController
+  DIR = "public/docs/"
+  HEALTH_FILE = "Health_n_Stuff.pdf"
+  DENTAL_FILE = "Dental_n_Stuff.pdf"
+  FILES = [ "#{DIR}#{HEALTH_FILE}", "#{DIR}#{DENTAL_FILE}" ]
 
   def index
     @benefits = Benefits.new
   end
 
   def download
-   begin
-     file = params[:name]
-     send_file file, :disposition => 'attachment'
-   rescue
-     redirect_to user_benefit_forms_path(:user_id => current_user.user_id)
-   end
+    begin
+      if FILES.include?(params[:name]) && params[:type] == "File"
+        path = Rails.root.join('public', 'docs', params[:name].split('/')[2])
+      else
+        path = Rails.root.join('public', 'docs', DENTAL_FILE)
+      end
+
+      send_file File.new(path), :disposition => 'attachment'
+    rescue
+      redirect_to user_benefit_forms_path(:user_id => current_user.user_id)
+    end
   end
 
   def upload
